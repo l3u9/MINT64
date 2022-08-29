b7149f390bcd4f64e3bb31911fe50628a0e42555
l3u9 <ksj31292000@gmail.com>
2022-08-29 15:33:29 +0900

    simple_bootloader

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,14 +3,53 @@
 
 SECTION .text
 
-mov ax, 0xB800
-mov ds, ax
+jmp 0x07c0:START ;copy 0x07c0 to CS segment and jump START LABLE
 
-mov byte [0x00] , 'M'
-mov byte [0x01] , 0x4A
+START:
+    mov ax, 0x07c0
+    mov ds, ax
+    mov ax, 0xb800
+    mov es, ax
+    
+    mov si, 0
+
+.SCREENCLEARLOOP:
+    mov byte [es: si], 0
+    
+    mov byte [es: si+1], 0x0a
+    
+    add si, 2
+
+    cmp si, 80*25*2
+
+    jl .SCREENCLEARLOOP
 
 
-jmp $   ; infinite loop in current position
+    mov si, 0
+    mov di, 0
+
+.MESSAGELOOP:
+    mov cl, byte [si + .MESSAGE1]
+    
+    cmp cl, 0
+    je .MESSAGEEND
+
+    mov byte [es: di], cl
+
+    add si, 1
+    add di, 2
+
+    jmp .MESSAGELOOP
+
+.MESSAGEEND:
+    jmp $
+
+.MESSAGE1:
+    db 'MINT64 OS Boot Loader Start~!!', 0
+
+
+
+
 
 times 510 - ( $ -$$ )   db 0x00 ; $: current line address
                                 ; $$: Start address of current Section(.text)
